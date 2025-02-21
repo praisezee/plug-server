8dd5989cb0330c84605444a85a39741e15ee84a0
dev_apus <folorunsopraise12@gmail.com>
2025-02-21 22:59:06 +0100

    done

--- controllers/kycController.js
+++ controllers/kycController.js
@@ -11,7 +11,7 @@ const businessKyc = async ( req, res ) =>
   if ( !name || !reg_type || !reg_date || !industry || !reg_address | !biz_address || !biz_city || !biz_state || !reg_city || !reg_state || !reg_number || !tax_number || !id_type || !email || !proof_address || !proof_id || !biz_cert || !mermat || !status || !principal_image || directors.length<1 ) return sendErrorResponse( res, 400, "Please fill all required fields" );
   try {
     const user = await prisma.user.findUniqueOrThrow( { where: { email } } );
-    console.log(directors[0])
+    console.log(directors[0].firstname)
     await prisma.businessKyc.create( {
       data: {
         userId: user.id,
